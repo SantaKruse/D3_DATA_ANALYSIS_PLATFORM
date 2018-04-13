a1a8d2a1b430f658378f033e467bcf30963376e9
SantaKruse <dkruse22@live.co.uk>
2018-04-13 20:23:39 +0100

    year toggle button bug fixed

--- app/assets/javascripts/viz_one.js
+++ app/assets/javascripts/viz_one.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     filterLists()
   })
 
+   $(document)
+  .on('click', '.btn', function() {
+    filterLists()
+  })
+
+
 
   function filterLists() {
     var form = $('#filter_form')
